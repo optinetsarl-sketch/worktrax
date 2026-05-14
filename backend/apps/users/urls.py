d0b1b9f103da510c2063e6dba47e69a7ac949533
Samsonwilson <hugueswilson16@gmail.com>
2026-05-14 08:35:46 -0400

    Auth correction

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 
 from .views.viewsLogin import LoginView, LogoutView
-from .views.viewsUser import UserListCreateView
+from .views.viewsUser import UserCreateView, UserListView
 from .views.viewsRole import RoleListCreateView
 
 urlpatterns = [
     path('login/', LoginView.as_view()),
     path('logout/', LogoutView.as_view(), name='logout'),
     path("roles/", RoleListCreateView.as_view()),
-    path("", UserListCreateView.as_view()),
+    path("create-user/", UserCreateView.as_view()),
+    path("usersliste/", UserListView.as_view()),
+    ### POUR ACTIVER OU DESACTIVER UN USER ###
+    # path('users/<int:pk>/toggle-active/', ToggleUserActiveView.as_view()),
      # path("", UserViewSet.as_view()),
     # path("", UserViewSet.as_view({'get': 'list', 'post': 'create'})),
 ]
--- backend/apps/users/views/viewsUser.py
+++ backend/apps/users/views/viewsUser.py
@@ -3,6 +3,8 @@ from rest_framework.permissions import IsAuthenticated
 from ..models import User
 from ..Serializers.user_serializer import UserSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
+
 # from ..permissions import HasTablePermission
 
 # class UserViewSet(viewsets.ModelViewSet):
@@ -10,7 +12,11 @@ from rest_framework import viewsets
 #     serializer_class = UserSerializer
     # permission_classes = [HasTablePermission]
 
-class UserListCreateView(generics.ListCreateAPIView):
+class UserCreateView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     # permission_classes = [IsAuthenticated]
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer    
+    permission_classes = [IsAdminUser]
